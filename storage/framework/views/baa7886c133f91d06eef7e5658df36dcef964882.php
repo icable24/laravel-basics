08bec9b6fa072cd1a6bfa387cc377b77a9e344dc
icable24 <icable130@gmail.com>
2017-01-06 00:04:59 +0800

    Final System

--- storage/framework/views/baa7886c133f91d06eef7e5658df36dcef964882.php
+++ storage/framework/views/baa7886c133f91d06eef7e5658df36dcef964882.php
@@ -9,7 +9,7 @@
         <span class="icon-bar"></span>
         <span class="icon-bar"></span>
       </button>
-      <a class="navbar-brand" href="<?php echo e(route('dashboard')); ?>">Brand</a>
+      <a class="navbar-brand" href="<?php echo e(route('dashboard')); ?>">Home</a>
     </div>
 
     <!-- Collect the nav links, forms, and other content for toggling -->
--- storage/framework/views/ff861f3f1a14de56ada432a165c42864a5e2ee65.php
+++ storage/framework/views/ff861f3f1a14de56ada432a165c42864a5e2ee65.php
@@ -1,19 +1,19 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title><?php echo $__env->yieldContent('title'); ?></title>
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-	<link rel="stylesheet" type="text/css" href="<?php echo e(URL::to('src/css/main.css')); ?>">
+    <title><?php echo $__env->yieldContent('title'); ?></title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+    <link rel="stylesheet" href="<?php echo e(URL::to('src/css/main.css')); ?>">
 </head>
 <body>
-	<?php echo $__env->make('includes.header', array_except(get_defined_vars(), array('__data', '__path')))->render(); ?>
-	<div class="container">
-		<?php echo $__env->yieldContent('content'); ?>
-	</div>
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
-<link rel="stylesheet" href="https://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/themes/smoothness/jquery-ui.css">
-<script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js"></script>
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
-<script src="<?php echo e(URL::to('src/js/app.js')); ?>"></script>
+    <?php echo $__env->make('includes.header', array_except(get_defined_vars(), array('__data', '__path')))->render(); ?>
+    <div class="container">
+        <?php echo $__env->yieldContent('content'); ?>
+    </div>
+
+    <script src="//code.jquery.com/jquery-1.12.0.min.js"></script>
+    <script src="//code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
+    <script src="<?php echo e(URL::to('src/js/app.js')); ?>"></script>
 </body>
 </html>
